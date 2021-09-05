0db968112743e0c4a01ed9abba31061fa52411ad
xMayank <mayankgupta.work@gmail.com>
2021-09-06 00:56:13 +0530

    structure changed again

--- index.js
+++ index.js
@@ -20,15 +20,7 @@ let response = {
   }),
 };
 
-async function generateReadMe(inputObj) {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
-    if (err) throw err;
-    const output = Mustache.render(data.toString(), inputObj);
-    fs.writeFileSync('README.md', output);
-  });
-}
-
-async function action() {
+async function setWeatherInformation() {
 
   let [error, data] = await utilities.safePromise(getWeatherInfo());
 
@@ -36,21 +28,37 @@ async function action() {
   response['city_weather'] = data.city_weather;
   response['sun_rise'] = data.sun_rise;
   response['sun_set'] = data.sun_set;
+}
+
+async function setInstagramPosts() {
+  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('johannesburginyourpocket', 3);
+  response.img1 = instagramImages[0];
+  response.img2 = instagramImages[1];
+  response.img3 = instagramImages[2];
+}
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), response);
+    fs.writeFileSync('README.md', output);
+  });
+}
+
+async function action() {
+
+  await setWeatherInformation();
 
   /**
    * Get pictures
    */
 
-  let imagesData = await puppeteerService.getLatestInstagramPostsFromAccount('johannesburginyourpocket', 3);
-
-  response['img1'] = imagesData[0];
-  response['img2'] = imagesData[1];
-  response['img3'] = imagesData[2];
+  await setInstagramPosts();
 
   /**
    * Generate README
    */
-  await generateReadMe(response);
+  await generateReadMe();
 
   /**
    * Fermeture de la boutique 👋
